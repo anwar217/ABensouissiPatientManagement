d4fd1f910204ed7badf0172872cfe611c6f6debb
AnwarBensouissi <anwar777bensouissi@gmail.com>
2022-12-12 10:09:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+class Patient{
+    constructor() {
+        this.patient = [];
+    }
+
+    list() {
+        return [...this.patient];
+    }
+
+    add(nom) {
+        let todo = {
+            nom: nom,
+            completed: false,
+        }
+
+        this.patient.push(todo);
+    }
+    complete(nom) {
+        let todoFound = false;
+        this.patient.forEach((todo) => {
+            if (todo.nom === nom) {
+                todo.completed = true;
+                todoFound = true;
+                return;
+            }
+        });
+
+        if (!todoFound) {
+            throw new Error(`No TODO was found with the title: "${nom}"`);
+        }
+    }
+
+}
+module.exports = Patient;
